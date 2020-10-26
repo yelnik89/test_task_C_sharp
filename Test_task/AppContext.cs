e5692d32956f3e172ae1a5c1cef829cd0c5ca86c
yelnik89 <denis.yelnik@icloud.com>
2020-10-27 00:36:20 +0700

    add model

---
+++ Test_task/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test_task
+{
+    class AppContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=123");
+        }
+    }
+}
